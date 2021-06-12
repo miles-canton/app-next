df43b191a59bd3e8535a5e3d95dc989d55add459
miles-canton <hmilyfe@163.com>
2021-06-12 20:08:49 +0800

    add test api

--- pages/index.js
+++ pages/index.js
@@ -20,6 +20,10 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
+        <p className={styles.description}>
+          Edited by {' '}
+          <code className={styles.code}>Miles Ji</code>
+        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
